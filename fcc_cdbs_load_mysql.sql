71501443d6d3c544edcc7d4011f6fca4e33108e4
Greg Elin <greg@fotonotes.net>
2011-11-24 22:06:04 -0500

    Find and fix all date fields previously missed. All date fields should now parse and load correctly.

--- fcc_cdbs_load_mysql.sql
+++ fcc_cdbs_load_mysql.sql
@@ -101,8 +101,9 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/app_party.dat'
 INTO TABLE app_party
 FIELDS TERMINATED BY '|'
-(application_id, party_id, party_type, cert_title, cert_date, other_fcc_id, party_notify_ind, party_relationship, sig_present_ind, sig_name, @last_change_date)
+(application_id, party_id, party_type, cert_title, @cert_date, other_fcc_id, party_notify_ind, party_relationship, sig_present_ind, sig_name, @last_change_date)
 set
+cert_date = str_to_date(@cert_date, '%m/%d/%Y'),
 last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 
 -- app_tracking
@@ -110,8 +111,13 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/app_tracking.dat'
 INTO TABLE app_tracking
 FIELDS TERMINATED BY '|'
-(application_id, app_status_date, cutoff_date, cutoff_type, cp_exp_date, app_status, dtv_checklist, amendment_stamped_date, accepted_date, tolling_code, @last_change_date)
+(application_id, @app_status_date, @cutoff_date, cutoff_type, @cp_exp_date, app_status, dtv_checklist, @amendment_stamped_date, @accepted_date, tolling_code, @last_change_date)
 set
+app_status_date = str_to_date(@app_status_date, '%m/%d/%Y'),
+cutoff_date = str_to_date(@cutoff_date, '%m/%d/%Y'),
+cp_exp_date = str_to_date(@cp_exp_date, '%m/%d/%Y'),
+amendment_stamped_date = str_to_date(@amendment_stamped_date, '%m/%d/%Y'),
+accepted_date = str_to_date(@accepted_date, '%m/%d/%Y'),
 last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 
 -- application
@@ -128,8 +134,9 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/call_sign_history.dat'
 INTO TABLE call_sign_history
 FIELDS TERMINATED BY '|'
-(begin_date, callsign, callsign_hist_id, callsign_seq_id, facility_id, @record_change_date)
+(@begin_date, callsign, callsign_hist_id, callsign_seq_id, facility_id, @record_change_date)
 set
+begin_date = str_to_date(@begin_date, '%m/%d/%Y'),
 record_change_date = str_to_date(@record_change_date, '%m/%d/%Y');
 
 -- dtv_agreement_group
@@ -171,13 +178,19 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/dtv_transition.dat'
 INTO TABLE dtv_transition
 FIELDS TERMINATED BY '|'
-(dtv_cur_channel, application_id, dtv_cur_na, ntsc_cur_channel, ntsc_cur_na, rep_max_cert, file_prefix_cer_a, app_arn_cer_a, file_prefix_cer_b, app_arn_cer_b, file_prefix_cer_d, app_arn_cer_d, rep_max_cer_type, file_prefix_cer_e, app_arn_cer_e, data_cert_ind, election, elected_channel_a, elected_channel_b, alt_elected_channel, intl_coord_ind, cont_election, cont_elected_channel, conflict_decision, reference_no, negot_agreemt_ind, tech_eng_ind, tent_chan_status, tentative_channel, post_tran_dtv_channel, pre_tran_dtv_channel, post_tran_auth_file_prefix, post_tran_auth_app_arn, post_tran_auth_not_yet_ind, @construction_deadline_dat, post_tran_operating_ind, op_status, op_expected_date, op_power_level, op_population_perc, construction_complete_ind, @expect_cons_complete_date, const_not_begun, const_underway, const_addl_needed, cp_status_ind, cp_mod_needed_ind, cp_mod_filed_ind, cp_mod_file_prefix, cp_mod_app_arn, cp_mod_filed_date, cp_mod_expected_date, addl_fcc_action_ind, addl_fcc_action_app_arn, addl_fcc_action_file_pref, addl_fcc_action_date, addl_intl_ind, addl_faa_approval_ind, addl_local_approval_ind, addl_equip_ind, addl_equip_new_ant_ind, addl_equip_adj_ant_ind, addl_equip_switch_ant_ind, addl_equip_new_trans_ind, addl_equip_adj_trans_ind, addl_equip_gen_install_ind, addl_equip_other_ind, addl_tower_ind, addl_coord_ind, addl_other_ind, analog_service_status, analog_status_ind, dtv_transition_plan_ind, addl_steps_ind, analog_nightlight_ind, analog_turnoff_pn_compl_flg, binding_termination_flg, termination_date_not_aff, termination_date_aff, aff_term_cert_flg, cont_analog_flg, phone_help_ctr_flg, walkin_help_ctr_flg, st_analog_nightlight_ind, @st_analog_nightlight_end_, inf_analog_nightlight_ind, inf_analog_nightlight_end, early_term_withdraw_ind, termination_time_0612_flg, termination_date_nce, termination_time_nce_flg, termination_time_notaff_flg, termination_time_aff_flg, nce_term_cert_flg, ref_contact_phone, ref_contact_hours, an_participation_notice_ind, an_eligibility_showing_ind, an_objection_to_elig_showg_ind, an_objection_callsign, an_revocation_ind, @last_update_date)
+(dtv_cur_channel, application_id, dtv_cur_na, ntsc_cur_channel, ntsc_cur_na, rep_max_cert, file_prefix_cer_a, app_arn_cer_a, file_prefix_cer_b, app_arn_cer_b, file_prefix_cer_d, app_arn_cer_d, rep_max_cer_type, file_prefix_cer_e, app_arn_cer_e, data_cert_ind, election, elected_channel_a, elected_channel_b, alt_elected_channel, intl_coord_ind, cont_election, cont_elected_channel, conflict_decision, reference_no, negot_agreemt_ind, tech_eng_ind, tent_chan_status, tentative_channel, post_tran_dtv_channel, pre_tran_dtv_channel, post_tran_auth_file_prefix, post_tran_auth_app_arn, post_tran_auth_not_yet_ind, @construction_deadline_dat, post_tran_operating_ind, op_status, @op_expected_date, op_power_level, op_population_perc, construction_complete_ind, @expect_cons_complete_date, const_not_begun, const_underway, const_addl_needed, cp_status_ind, cp_mod_needed_ind, cp_mod_filed_ind, cp_mod_file_prefix, cp_mod_app_arn, @cp_mod_filed_date, @cp_mod_expected_date, addl_fcc_action_ind, addl_fcc_action_app_arn, addl_fcc_action_file_pref, @addl_fcc_action_date, addl_intl_ind, addl_faa_approval_ind, addl_local_approval_ind, addl_equip_ind, addl_equip_new_ant_ind, addl_equip_adj_ant_ind, addl_equip_switch_ant_ind, addl_equip_new_trans_ind, addl_equip_adj_trans_ind, addl_equip_gen_install_ind, addl_equip_other_ind, addl_tower_ind, addl_coord_ind, addl_other_ind, analog_service_status, analog_status_ind, dtv_transition_plan_ind, addl_steps_ind, analog_nightlight_ind, analog_turnoff_pn_compl_flg, binding_termination_flg, @termination_date_not_aff, @termination_date_aff, aff_term_cert_flg, cont_analog_flg, phone_help_ctr_flg, walkin_help_ctr_flg, st_analog_nightlight_ind, @st_analog_nightlight_end_, inf_analog_nightlight_ind, inf_analog_nightlight_end, early_term_withdraw_ind, termination_time_0612_flg, @termination_date_nce, termination_time_nce_flg, termination_time_notaff_flg, termination_time_aff_flg, nce_term_cert_flg, ref_contact_phone, ref_contact_hours, an_participation_notice_ind, an_eligibility_showing_ind, an_objection_to_elig_showg_ind, an_objection_callsign, an_revocation_ind, @last_update_date)
 set
-last_update_date = str_to_date(@last_update_date, '%m/%d/%Y'),
 construction_deadline_dat = str_to_date(@construction_deadline_dat, '%m/%d/%Y'),
+op_expected_date = str_to_date(@op_expected_date, '%m/%d/%Y'),
 expect_cons_complete_date = str_to_date(@expect_cons_complete_date, '%m/%d/%Y'),
+cp_mod_filed_date = str_to_date(@cp_mod_filed_date, '%m/%d/%Y'),
+cp_mod_expected_date = str_to_date(@cp_mod_expected_date, '%m/%d/%Y'),
+addl_fcc_action_date = str_to_date(@addl_fcc_action_date, '%m/%d/%Y'),
+termination_date_not_aff = str_to_date(@termination_date_not_aff, '%m/%d/%Y'),
+termination_date_aff = str_to_date(@termination_date_aff, '%m/%d/%Y'),
 st_analog_nightlight_end_ = str_to_date(@st_analog_nightlight_end_, '%m/%d/%Y'),
-inf_analog_nightlight_end = str_to_date(@inf_analog_nightlight_end, '%m/%d/%Y');
+termination_date_nce = str_to_date(@termination_date_nce, '%m/%d/%Y'),
+last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 
 -- elevation_ant_make
 -- load elevation_ant_make
@@ -271,8 +284,11 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/if_notification.dat'
 INTO TABLE if_notification
 FIELDS TERMINATED BY '|'
-(application_id, broadcast_date, ibiquity_cert, tech_name, tech_phone, combined_power, analog_power, digital_power, analog_remain_ind, env_excluded_ind, digital_power_reduced, digital_power_reduced_na, ref_file_prefix, ref_app_arn, test_date, resolved_date, analog_erp, digital_erp, @last_change_date)
+(application_id, @broadcast_date, ibiquity_cert, tech_name, tech_phone, combined_power, analog_power, digital_power, analog_remain_ind, env_excluded_ind, digital_power_reduced, digital_power_reduced_na, ref_file_prefix, ref_app_arn, @test_date, @resolved_date, analog_erp, digital_erp, @last_change_date)
 set
+broadcast_date = str_to_date(@broadcast_date, '%m/%d/%Y'),
+test_date = str_to_date(@test_date, '%m/%d/%Y'),
+resolved_date = str_to_date(@resolved_date, '%m/%d/%Y'),
 last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 
 -- int_translator
@@ -289,8 +305,14 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/intl_tracking.dat'
 INTO TABLE intl_tracking
 FIELDS TERMINATED BY '|'
-(accepted_date, application_id, can_coord_status, change_list_date, change_list_num, hours_operation, ifrb_date, ifrb_list_flg, ifrb_serial_num, intl_class, itu_coord_status, mex_coord_status, neg_allot_ind, notified_pattern, notified_status, proposed_date, referred_date, region_2_status, updating_agency, notified_date, intl_status, @last_change_date)
+(@accepted_date, application_id, can_coord_status, @change_list_date, change_list_num, hours_operation, @ifrb_date, ifrb_list_flg, ifrb_serial_num, intl_class, itu_coord_status, mex_coord_status, neg_allot_ind, notified_pattern, notified_status, @proposed_date, @referred_date, region_2_status, updating_agency, @notified_date, intl_status, @last_change_date)
 set
+accepted_date = str_to_date(@accepted_date, '%m/%d/%Y'),
+change_list_date = str_to_date(@change_list_date, '%m/%d/%Y'),
+ifrb_date = str_to_date(@ifrb_date, '%m/%d/%Y'),
+proposed_date = str_to_date(@proposed_date, '%m/%d/%Y'),
+referred_date = str_to_date(@referred_date, '%m/%d/%Y'),
+notified_date = str_to_date(@notified_date, '%m/%d/%Y'),
 last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 
 -- lic_app_indicators
@@ -298,18 +320,21 @@ last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/lic_app_indicators.dat'
 INTO TABLE lic_app_indicators
 FIELDS TERMINATED BY '|'
-(application_id, auth_retrans_ind, chng_transm_pwr_ind, cp_stmt_incorrect_ind, cp_terms_met_ind, da_inst_compl_ind, da_inst_correct_ind, decr_c_erp_ind, decr_nc_erp_ind, del_cntr_prot_ind, incr_erp_221_ind, incr_erp_dckt_ind, incr_erp_ind, incr_erp_ownr_ind, lic_app_service, lic_pta_ind, nce_to_comm_ind, no_intl_coord_ind, orig_cp_file_prefix, orig_cp_app_arn, lic_file_prefix, lic_app_arn, rad_astr_not_ind, repl_da_compl_ind, repl_da_ind, rule_73_1620a1_ind, rule_73_1675a3_ind, rule_73_1675a_ind, rule_73_1690c8i_ind, rule_73_1690c_ind, rule_73_1692_ind, rule_73_207_ind, rule_73_315ab_ind, rule_73_525a1_ind, rule_73_68_ind, spec_op_conds_ind, sta_id_rules_ind, stat_ready_ind, trans_auth_erp_ind, type_appr_stereo_ind, inst_ht_dif_ok_ind, rule_73_1590, rule_73_1660_ind, rule_73_1675c_1_ind, rule_73_1675c_ind, rule_73_1690c9_ind, rule_73_3555_ind, rule_73_685f_ind, rule_73_68d3_ind, sales_type_flg, conv_lptv_cp_arn, conv_lptv_cp_prefix, conv_lptv_lic_arn, conv_lptv_lic_prefix, displaced_cp_filed_date, no_change_lp_cp_ind, rule_73_6011_ind, rule_73_6012_ind, rule_73_6013_ind, rule_73_6020_ind, no_change_to_classa_cp_in, pta_type, repl_trans_line_ind, rule_73_850b_ind, rule_73_1675a_nc_ind, rule_73_1675a_ax_ind, @orig_cp_app_expiration_date, @record_change_date)
+(application_id, auth_retrans_ind, chng_transm_pwr_ind, cp_stmt_incorrect_ind, cp_terms_met_ind, da_inst_compl_ind, da_inst_correct_ind, decr_c_erp_ind, decr_nc_erp_ind, del_cntr_prot_ind, incr_erp_221_ind, incr_erp_dckt_ind, incr_erp_ind, incr_erp_ownr_ind, lic_app_service, lic_pta_ind, nce_to_comm_ind, no_intl_coord_ind, orig_cp_file_prefix, orig_cp_app_arn, lic_file_prefix, lic_app_arn, rad_astr_not_ind, repl_da_compl_ind, repl_da_ind, rule_73_1620a1_ind, rule_73_1675a3_ind, rule_73_1675a_ind, rule_73_1690c8i_ind, rule_73_1690c_ind, rule_73_1692_ind, rule_73_207_ind, rule_73_315ab_ind, rule_73_525a1_ind, rule_73_68_ind, spec_op_conds_ind, sta_id_rules_ind, stat_ready_ind, trans_auth_erp_ind, type_appr_stereo_ind, inst_ht_dif_ok_ind, rule_73_1590, rule_73_1660_ind, rule_73_1675c_1_ind, rule_73_1675c_ind, rule_73_1690c9_ind, rule_73_3555_ind, rule_73_685f_ind, rule_73_68d3_ind, sales_type_flg, conv_lptv_cp_arn, conv_lptv_cp_prefix, conv_lptv_lic_arn, conv_lptv_lic_prefix, @displaced_cp_filed_date, no_change_lp_cp_ind, rule_73_6011_ind, rule_73_6012_ind, rule_73_6013_ind, rule_73_6020_ind, no_change_to_classa_cp_in, pta_type, repl_trans_line_ind, rule_73_850b_ind, rule_73_1675a_nc_ind, rule_73_1675a_ax_ind, @orig_cp_app_expiration_date, @record_change_date)
 set
-record_change_date = str_to_date(@record_change_date, '%m/%d/%Y'),
-orig_cp_app_expiration_date = str_to_date(@orig_cp_app_expiration_date, '%m/%d/%Y');
+displaced_cp_filed_date = str_to_date(@displaced_cp_filed_date, '%m/%d/%Y'),
+orig_cp_app_expiration_date = str_to_date(@orig_cp_app_expiration_date, '%m/%d/%Y'),
+record_change_date = str_to_date(@record_change_date, '%m/%d/%Y');
+
 
 -- nce_factors
 -- load nce_factors
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/nce_factors.dat'
 INTO TABLE nce_factors
 FIELDS TERMINATED BY '|'
-(application_id, claimed_points, existing_auths, existing_cps, existing_fillin_auths, existing_fillin_cps, fill_in_serv_ind, gain_area, population_served, rule_73_313_c_ind, rule_73_3555_ind, rule_73_7000_ind, rule_73_7003_b_c_ind, technical_points, total_points, divers_own_ind, min_first_service_ind, min_sec_service_ind, supplement_date, first_aural_service_ind, first_tribal_service_ind, @last_change_date)
+(application_id, claimed_points, existing_auths, existing_cps, existing_fillin_auths, existing_fillin_cps, fill_in_serv_ind, gain_area, population_served, rule_73_313_c_ind, rule_73_3555_ind, rule_73_7000_ind, rule_73_7003_b_c_ind, technical_points, total_points, divers_own_ind, min_first_service_ind, min_sec_service_ind, @supplement_date, first_aural_service_ind, first_tribal_service_ind, @last_change_date)
 set
+supplement_date = str_to_date(@supplement_date, '%m/%d/%Y'),
 last_change_date = str_to_date(@last_change_date, '%m/%d/%Y');
 
 -- ownership_capitalization
@@ -326,8 +351,10 @@ last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/ownership_contract.dat'
 INTO TABLE ownership_contract
 FIELDS TERMINATED BY '|'
-(ownership_contract_id, application_id, contract_description, contract_person, execution_date, expiration_date, order_number, agreement_type_lma, agreement_type_naa, agreement_type_oth, execution_date_month, execution_date_year, expiration_date_month, expiration_date_year, expiration_date_na, @last_update_date)
+(ownership_contract_id, application_id, contract_description, contract_person, @execution_date, @expiration_date, order_number, agreement_type_lma, agreement_type_naa, agreement_type_oth, execution_date_month, execution_date_year, expiration_date_month, expiration_date_year, expiration_date_na, @last_update_date)
 set
+execution_date = str_to_date(@execution_date, '%m/%d/%Y'),
+expiration_date = str_to_date(@expiration_date, '%m/%d/%Y'),
 last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 
 -- ownership_exemptions
@@ -390,8 +417,9 @@ last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/ownership_report.dat'
 INTO TABLE ownership_report
 FIELDS TERMINATED BY '|'
-(application_id, accurate_date, non_attributable_ind, attributable_ind, related_ind, exemption_ind, owner_type, entity_control_ind, separate_form_ind, attrib_exemption_ind, capitalization_na, contract_info_na, individuals_related_ind, lic_frn, lic_name, org_chart_na, respondent_nature_flg, resp_interests_na, @last_update_date)
+(application_id, @accurate_date, non_attributable_ind, attributable_ind, related_ind, exemption_ind, owner_type, entity_control_ind, separate_form_ind, attrib_exemption_ind, capitalization_na, contract_info_na, individuals_related_ind, lic_frn, lic_name, org_chart_na, respondent_nature_flg, resp_interests_na, @last_update_date)
 set
+accurate_date = str_to_date(@accurate_date, '%m/%d/%Y'),
 last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 
 -- ownership_structure
@@ -426,8 +454,9 @@ last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 LOAD DATA LOCAL INFILE '/codedata/FCC/data/public/cdbs/supp_facility.dat'
 INTO TABLE supp_facility
 FIELDS TERMINATED BY '|'
-(parent_facility_id, supp_type, supp_callsign, supp_fac_status, status_date, site_number, fac_channel, comm_city, comm_state, @last_update_date)
+(parent_facility_id, supp_type, supp_callsign, supp_fac_status, @status_date, site_number, fac_channel, comm_city, comm_state, @last_update_date)
 set
+status_date = str_to_date(@status_date, '%m/%d/%Y'),
 last_update_date = str_to_date(@last_update_date, '%m/%d/%Y');
 
 -- tv_app_indicators
